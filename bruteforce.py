961f8f71d62910fde9abfebec9d8da906895e380
MosheHz <m.n.heifetz@gmail.com>
2025-12-21 05:24:44 +0200

    Add files via upload
    i added passwords creator that base on the user name for medium passwords, and passwords creator for strong passwords

--- bruteforce.py
+++ bruteforce.py
@@ -1,14 +1,21 @@
-from util import users_list
-import requests, time, json
+from util import users_list, generate_sequences
+from passwords import *
+import requests, time, yaml
 
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+PASS_LEVEL = config["config"]["pass_level"]
 
 LOGIN_URL = "http://127.0.0.1:5000/login"
-USERS = users_list()
-print(USERS)
-PASSWORDS_FILE = ""
+
+USERS = users_list(PASS_LEVEL)
+
+PASS_FILE = f"passwords/{PASS_LEVEL}pass.txt"
+
 
 for user in USERS:
-    with open ("passwords/weakpasswords.txt") as f:
+    generate_sequences(PASS_LEVEL, PASS_FILE, user, 5)
+    with open(PASS_FILE, "r") as f:
         for attempt_num, password in enumerate(f, start=1):
             password = password.strip()
 
@@ -23,7 +30,8 @@ for user in USERS:
             if response.status_code == 200:
                 break
             
-            if attempt_num % 50000 == 0:
+            if attempt_num % 500000 == 0:
+                print("next user")
                 pass
 
-            time.sleep(0.05)
+            time.sleep(0.01)
--- util.py
+++ util.py
@@ -1,4 +1,7 @@
-import json
+import itertools, string, random, json
+
+
+MAX_PASS = 300000
 
 
 def users_pass_list():
@@ -16,19 +19,60 @@ def users_pass_list():
 
     return users
 
-def users_list():
+
+def users_list(category):
     with open("json/user.json", "r") as f:
         data = json.load(f)
     
     users = []
 
-    for catigory, user_list in data.items():
-        for user in user_list:
+    if category in data:
+        print(category)
+        for user in data[category]:
             users.append(
                 user['username']
             )
-
     return users
 
 
-            
+def med_generate_sequences(file, name, min_length=4, max_length=6):
+    name_lower = name.lower()
+    count = 0 
+    print(name_lower)
+    substring = [name_lower[:i] for i in range(1, len(name_lower) + 1)]
+    numbers = '0123456789'
+    with open(file, "w") as f:
+        for sub in substring:
+            sub_len = len(sub)
+            for leng in range(min_length, max_length + 1):
+                if count > MAX_PASS:
+                    break
+                num_digits = leng - sub_len
+                if num_digits > 0:
+                    for num_combo in itertools.product(numbers, repeat=num_digits):
+                        num_str = ''.join(num_combo)
+                        f.write(sub + num_str + '\n')
+                        f.write(num_str + sub + '\n')
+                        count += 2
+                else:
+                    f.write(sub + '\n')
+                    count += 1
+
+
+def rand_generate_sequences(file):
+    length = random.choice([8, 9])
+
+    characters = string.ascii_letters + string.digits
+
+    with open(file, "w") as f:
+        for i in range(100000):
+            random_text = ''.join(random.choice(characters) for _ in range(length))
+            f.write(random_text + '\n')
+
+
+def generate_sequences(type, file, name=None, min_len=None):
+    if type == "mudium":
+        med_generate_sequences(file, name, min_len)
+    else:
+        rand_generate_sequences(file)
+       
