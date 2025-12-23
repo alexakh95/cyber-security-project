924d99355f8158fa88a824267cc684dde77b95bc
Sasha <hlivnenko777@gmail.com>
2025-12-23 19:42:26 +0200

    [report generation] created the report generation for security event

--- util.py
+++ util.py
@@ -1,4 +1,16 @@
-import json
+import json,yaml
+from datetime import datetime
+
+#open config file
+with open("config.yaml", "r") as f:
+    APP_CONFIG = yaml.safe_load(f)
+
+GROUP_SEED = APP_CONFIG["group_seed"]
+HASH_METHOD = APP_CONFIG["config"]["hash"]
+SALT = APP_CONFIG["config"]["salt"]
+PEPPER = APP_CONFIG["config"]["pepper"]
+PROTECTION = APP_CONFIG["protection"]
+ATTACK = APP_CONFIG["attack"]
 
 
 def users_pass_list():
@@ -30,8 +42,19 @@ def users_list():
 
     return users
 
-
-def log_attempt(data,log_file=""):
-    with open(log_file, "a", encoding="utf-8") as f:
-        json.dump(data, f)
-        f.write("\n")         
+def log_security_event(username,result,latency_ms):
+    filename = f"json_logs/logs_{HASH_METHOD}_{PROTECTION}_{ATTACK}.json"
+    
+    log_entry = {
+            "timestamp": datetime.utcnow().isoformat() + "Z",
+            "username": username,
+            "result":result,
+            "latency_ms": latency_ms,
+            "hash_mode": HASH_METHOD,
+            "prot_flag": PROTECTION,
+            "group_seed": GROUP_SEED
+        }
+    
+    with open(filename, "a") as f:
+        f.write(json.dumps(log_entry) + "\n")
+       
